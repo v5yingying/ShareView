fa5b7cf533f83619e362ecd86796b35c3fc6d1d1
youruogege <v5yingying@163.com>
2016-12-20 11:41:33 +0800

    create button

---
+++ YYShake/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YYShake
+//
+//  Created by caiyanying on 16/12/20.
+//  Copyright © 2016年 youruogege. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ YYShake/YRShareView.h
@@ -0,0 +1,13 @@
+//
+//  YRShareView.h
+//  YYShake
+//
+//  Created by caiyanying on 16/12/20.
+//  Copyright © 2016年 youruogege. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface YRShareView : UIView
+
+@end
